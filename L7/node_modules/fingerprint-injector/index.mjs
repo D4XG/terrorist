7753602086ec949763904dad6a1d57dbd24a3b76
DAXG <108587935+D4XG@users.noreply.github.com>
2024-01-28 22:38:47 +0700

    Panel Optimization

--- L7/node_modules/fingerprint-injector/index.mjs
+++
@@ -1,6 +0,0 @@
-import mod from "./index.js";
-
-export default mod;
-export const FingerprintInjector = mod.FingerprintInjector;
-export const newInjectedContext = mod.newInjectedContext;
-export const newInjectedPage = mod.newInjectedPage;
