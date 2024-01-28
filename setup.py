9cec986c2c421045001dbb2ae8496236dead20e6
DAXG <108587935+D4XG@users.noreply.github.com>
2024-01-28 22:50:10 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,80 @@
 import subprocess
 import os
 
-def install_python_requirements():
+def install_python_packages():
     try:
-        with open('requirements.txt', 'r') as file:
-            for line in file:
-                package = line.strip()
-                subprocess.check_call(['pip', 'install', package])
-                print(f"Python package '{package}' installed successfully!")
+        # Install additional Python packages
+        subprocess.check_call(['pip', 'install', 'termcolor'])
+
+        subprocess.check_call(['pip', 'install', 'colorama'])
+
+        subprocess.check_call(['pip', 'install', 'datetime'])
+
+        subprocess.check_call(['pip', 'install', 'cfscrape'])
+
+        subprocess.check_call(['pip', 'install', 'socks'])
+
+        subprocess.check_call(['pip', 'install', 'pysocks'])
+
+        subprocess.check_call(['pip', 'install', 'cloudscraper'])
+
+        subprocess.check_call(['pip', 'install', 'pystyle'])
+
+        subprocess.check_call(['pip', 'install', 'requests'])
+
+        subprocess.check_call(['pip', 'install', 'scapy'])
+
+        subprocess.check_call(['pip', 'install', 'cython'])
+
+        subprocess.check_call(['pip', 'install', 'darksky'])
+
+        subprocess.check_call(['pip', 'install', 'darkskylib'])
+
+        subprocess.check_call(['pip', 'install', 'termcolor'])
+
+        subprocess.check_call(['pip', 'install', 'aiohttp_socks'])
+
         print("All Python packages installed successfully!")
     except subprocess.CalledProcessError as e:
         print(f"Error: Failed to install Python packages - {e}")
 
+        # Go to AA
+        os.chdir('AA')
+
+        # Install additional Python packages
+        subprocess.check_call(['pip', 'install', 'termcolor'])
+
+        subprocess.check_call(['pip', 'install', 'colorama'])
+
+        subprocess.check_call(['pip', 'install', 'datetime'])
+
+        subprocess.check_call(['pip', 'install', 'cfscrape'])
+
+        subprocess.check_call(['pip', 'install', 'socks'])
+
+        subprocess.check_call(['pip', 'install', 'pysocks'])
+
+        subprocess.check_call(['pip', 'install', 'cloudscraper'])
+
+        subprocess.check_call(['pip', 'install', 'pystyle'])
+
+        subprocess.check_call(['pip', 'install', 'requests'])
+
+        subprocess.check_call(['pip', 'install', 'scapy'])
+
+        subprocess.check_call(['pip', 'install', 'cython'])
+
+        subprocess.check_call(['pip', 'install', 'darksky'])
+
+        subprocess.check_call(['pip', 'install', 'darkskylib'])
+
+        subprocess.check_call(['pip', 'install', 'termcolor'])
+
+        subprocess.check_call(['pip', 'install', 'aiohttp_socks'])
+
+        print("All Python packages installed successfully!")
+        os.chdir('..')
+        
 def install_npm_packages():
     npm_commands = [
         'npm i user-agents', 'npm i header-generator', 'npm i request', 'npm i fake-useragent',
@@ -20,7 +83,7 @@ def install_npm_packages():
         'npm i fingerprint-generator', 'npm i fingerprint-injector', 'npm i ua-parser-js', 'npm i http2',
         'npm i minimist', 'npm i socks', 'npm i puppeteer', 'npm i hcaptcha-solver', 'npm i puppeteer-extra',
         'npm i puppeteer-extra-plugin-recaptcha', 'npm i puppeteer-extra-plugin-stealth', 'npm i http', 'npm i http2',
-        'npm i zombie', 'npm i random-referer', 'npm i jar', 'npm i child_process'
+        'npm i zombie', 'npm i random-referer', 'npm i jar'
     ]
     
     try:
@@ -36,12 +99,15 @@ def install_npm_packages():
             subprocess.run(command, shell=True, check=True)
             print(f"NPM package '{command}' installed successfully in VIP folder!")
 
+        # Change directory back to the main directory
+        os.chdir('..')
+
         print("All NPM packages installed successfully!")
     except subprocess.CalledProcessError as e:
         print(f"Error: Failed to install NPM package - {e}")
 
 def setup():
-    install_python_requirements()
+    install_python_packages()
     install_npm_packages()
 
 if __name__ == "__main__":
