33fcd8f124a9a2d555b4faba972f8c3ef3a63dac
Kieu Duy Anh <kieuduyanh@protonmail.com>
2024-01-30 06:09:56 +0000

    setup

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ def install_python_packages():
 
         subprocess.check_call(['pip', 'install', 'datetime'])
 
+        subprocess.check_call(['pip', 'install', 'opencv-python'])
+
         subprocess.check_call(['pip', 'install', 'cfscrape'])
 
         subprocess.check_call(['pip', 'install', 'socks'])
